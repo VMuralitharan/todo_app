598f6295919ab2482bd0c53c0a7c9c56936c76e5
VMuralitharan <vijayakumarmuralitharan@gmail.com>
2025-07-16 23:37:07 +0530

    stateless widget created

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(Mainapp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class Mainapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return MaterialApp(home: Scaffold());
   }
 }
