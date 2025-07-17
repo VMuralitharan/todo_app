9e9a39bb224f7d59dfeafb51a1606c6b11956914
VMuralitharan <vijayakumarmuralitharan@gmail.com>
2025-07-17 14:15:01 +0530

    hii

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,26 @@ void main() {
 class Mainapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold());
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("ToDo", style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.black,
+        ),
+        body: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(15),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  label: Text("Enter a task"),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
